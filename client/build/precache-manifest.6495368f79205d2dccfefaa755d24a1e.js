35b705132d23424e80933307e15b47dd2ea47230
Mark Techson <2554588+MarkTechson@users.noreply.github.com>
2019-02-14 21:44:19 -0600

    more changes

---
+++ client/build/precache-manifest.6495368f79205d2dccfefaa755d24a1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5dfd14066e909474a8a4",
+    "url": "/static/js/main.5dfd1406.chunk.js"
+  },
+  {
+    "revision": "889d935ebba549506f5e",
+    "url": "/static/js/2.889d935e.chunk.js"
+  },
+  {
+    "revision": "5dfd14066e909474a8a4",
+    "url": "/static/css/main.b2f3f0db.chunk.css"
+  },
+  {
+    "revision": "821e7996f9c4458039e6b2b0cafb0f5e",
+    "url": "/index.html"
+  }
+];
